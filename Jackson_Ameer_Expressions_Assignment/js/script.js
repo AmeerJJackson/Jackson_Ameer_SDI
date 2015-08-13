5e9770ba4f2d64501df4c12d35d003c7d330624f
AmeerJJackson <m388@ymail.com>
2015-08-13 03:00:29 -0700

    My first code added comments
    Bachelor grocery list.

--- Jackson_Ameer_Expressions_Assignment/js/script.js
+++ Jackson_Ameer_Expressions_Assignment/js/script.js
@@ -14,22 +14,28 @@ var sandwhichmeat = 6;
 var aquafinabottlewater = 6;
 
 //two boxes of cereal
-var cereal = 5;
+var cereal = [5, 5 * 2];
 
 //two loafs of bread
-var bread = 1;
+var bread = [1, 1 * 2];
 
 //top ramon noodles come in a 12 pack or .17 cent each
-var topramonnoodles = [.17, .17 * 12];  
+var topramennoodles = [.17, .17 * 12];  
 
 //lemonade is 2 for $5.00
 var juice = [2.50, 2.50 * 2]; 
 
 
-var sum = vitamindmilk + cereal * 2 + eggs + bread * 2 + bestfoodsmayonnaise + kraftsinglescheese + sandwhichmeat + topramonnoodles [1] + aquafinabottlewater + juice [1];
+var sum = vitamindmilk + cereal [1] + eggs + bread [1] + bestfoodsmayonnaise + kraftsinglescheese + sandwhichmeat + topramennoodles [1] + aquafinabottlewater + juice [1];
 
 
 //california sales tax is 7.5% 
 var salestax = 0.075
 var total = sum * salestax + sum;
 console.log(total);
+
+//If your purchace everything your total will be $50.193
+//If you purchase everything, with one box of cereal your total will be $45.193
+//If you purchase everything, with one loaf of bread your total will be $49.493
+//If you purchase everything, with one topramennoodle your total will be $48.55775
+//If you purchase everything, with one lemonade your total will be $47.8805
