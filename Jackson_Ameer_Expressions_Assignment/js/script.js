ac2ebc3c86ca854f7f3bf66fb83c13f7312f5023
AmeerJJackson <m388@ymail.com>
2015-08-13 20:27:02 -0700

    My first code Final
    Bachelor grocery list Final

--- Jackson_Ameer_Expressions_Assignment/js/script.js
+++ Jackson_Ameer_Expressions_Assignment/js/script.js
@@ -4,9 +4,9 @@
 
 alert("Give me an A, I did my best!")
 
-//Grocery list
+//Bachelor grocery list
 
-var vitamindmilk = 3;
+var vitaminDmilk = 3;
 var eggs  = 6;
 var bestfoodsmayonnaise = 4;
 var kraftsinglescheese = 3;
@@ -20,13 +20,13 @@ var cereal = [5, 5 * 2];
 var bread = [1, 1 * 2];
 
 //top ramon noodles come in a 12 pack or .17 cent each
-var topramennoodles = [.17, .17 * 12];  
+var topramennoodles = [.17 * 0, .17 * 1, .17 * 2, .17 * 3, .17 * 4, .17 * 5, .17 * 6, .17 * 7, .17 * 8, .17 * 9, .17 * 10, .17 * 11, .17 * 12];  
 
 //lemonade is 2 for $5.00
 var juice = [2.50, 2.50 * 2]; 
 
 
-var sum = vitamindmilk + cereal [1] + eggs + bread [1] + bestfoodsmayonnaise + kraftsinglescheese + sandwhichmeat + topramennoodles [1] + aquafinabottlewater + juice [1];
+var sum = vitamindmilk + cereal [0] + eggs + bread [1] + bestfoodsmayonnaise + kraftsinglescheese + sandwhichmeat + topramennoodles [12] + aquafinabottlewater + juice [1];
 
 
 //california sales tax is 7.5% 
